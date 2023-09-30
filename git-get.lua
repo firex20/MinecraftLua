95cdaac316f7480b503a3087338f01856afac64c
firex20 <molden14@gmail.com>
2023-09-30 17:18:27 +0200

    actualizado script git-get

--- git-get.lua
+++ git-get.lua
@@ -1,6 +1,7 @@
 local repo = "https://raw.githubusercontent.com/firex20/MinecraftLua/main/"
 local fullfile = repo..arg[1]
-if fs.exists(./arg[1])
+local curdir = shell.dir()
+if fs.exists(curdir.."/"..arg[1])
 then
     print ("El archivo ya existe ¿Sobrescribir? Y/N [Y]:")
     local confir = io.read()
