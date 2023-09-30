d8a41c09e24075d3235ca74dfe67f0d4a09e7af1
firex20 <molden14@gmail.com>
2023-09-30 17:21:36 +0200

    actualizado script git-get

--- git-get.lua
+++ git-get.lua
@@ -5,10 +5,12 @@ if fs.exists(curdir.."/"..arg[1])
 then
     print ("El archivo ya existe ¿Sobrescribir? Y/N [Y]:")
     local confir = io.read()
-    if confir == nil or confir == "Y"
+    if confir == "" or confir == "Y"
     then
         shell.run("rm", "./"..arg[1])
         shell.run("wget", fullfile, arg[1])
+    else
+        print("Descarga abortada!")
     end
 else
     shell.run("wget", fullfile, arg[1])
